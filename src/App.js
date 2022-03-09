11a127227e47e176f550e1c86c699944f3edae1c
Deymer Espinosa <deymerespinosap@gmail.com>
2022-03-09 17:02:20 -0500

    modified: package.json, App.js, Carrito y registro

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-app.use(express.static('public'));
+
+app.use(express.static(path.join(__dirname,"../","public")));
 
 app.listen(3000, ()=>{
     console.log('Servidor funcionando');
@@ -26,3 +28,4 @@ app.get('/cart', (req,res)=>{
 app.get('/detail', (req,res)=>{
   res.sendFile(__dirname + '/views/productDetail.html');
 });
+
