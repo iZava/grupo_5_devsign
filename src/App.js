09b8acafb89910712113bd5e9630b7083521f558
iZava <ignaciomzavalla@gmail.com>
2022-03-18 09:03:00 -0300

    modified PORT

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port= process.env.PORT || 9000
+const PORT= process.env.PORT || 9000
 
 app.use(express.static(path.join(__dirname, "../", "public")));
 
