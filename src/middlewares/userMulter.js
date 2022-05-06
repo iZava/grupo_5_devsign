44924eef99743cf59e0489214f01579f9287f8a9
iZava <ignaciomzavalla@gmail.com>
2022-05-06 18:54:34 -0300

    carpeta middlewares

---
+++ src/middlewares/userMulter.js
@@ -0,0 +1,19 @@
+const path = require("path");
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.resolve(__dirname, "../../public/images/users"));
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(
+      null,
+      file.fieldname + "-" + uniqueSuffix + path.extname(file.originalname)
+    );
+  },
+});
+
+const upload = multer({ storage: storage });
+
+module.exports = upload;
---
+++ src/middlewares/validateRegister.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const { body } = require("express-validator"); 
+
+const validations = [
+    body("nombre").notEmpty().withMessage("El nombre es obligatorio"),
+    body("logUser").notEmpty().withMessage("El nombre de usuario es obligatorio"),
+    body("email").notEmpty().withMessage("El email es obligatorio").bail().isEmail().withMessage("Debes escribir el email en un formato válido"),
+    body("password").notEmpty().withMessage("La contraseña es obligatoria").bail().isLength({ min: 6 }).withMessage("La contraseña debe tener al menos 6 caracteres"),
+    body("repeat_password").notEmpty().withMessage("Debe repetir su contraseña").custom((value, { req }) => {
+        if (value !== req.body.password) {
+            throw new Error("Las contraseñas no coinciden");
+        }
+        return true;
+    }),
+    body("image").custom((value, { req }) => {
+        let file = req.file;
+        let acceptedExtensions = [".jpg", ".jpeg", ".png", ".JPG", ".JPEG", ".PNG"];
+        if (!file) {
+            throw new Error("Debes subir una imagen");
+        } else { 
+        let fileExtension = path.extname(file.originalname);
+        if (!acceptedExtensions.includes(fileExtension)) {
+            throw new Error(`Solo se admiten formatos de imagen: ${acceptedExtensions.join(", ")}`);
+        }
+      }
+        return true;
+    }),
+];
+
+    module.exports = validations;
