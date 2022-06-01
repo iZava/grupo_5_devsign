1ccc06630043b2267634471f64758732e0552939
iZava <ignaciomzavalla@gmail.com>
2022-06-01 18:47:14 -0300

    Sequelize config

---
+++ database/config/config.js
@@ -0,0 +1,25 @@
+module.exports ={
+  "development": {
+    "username": "root",
+    "password": "devsign2022",
+    "database": "devsign_db",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": "devsign2022",
+    "database": "devsign_db",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": "devsign2022",
+    "database": "devsign_db",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+} 
+
+//* Al final del archivo index.js (/database/models/index.js) encontraremos la expórtación del objeto DB
