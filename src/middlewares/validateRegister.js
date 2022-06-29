10520cdc80f9e4b1571d243df3564c1d789767e6
iZava <ignaciomzavalla@gmail.com>
2022-06-29 16:50:41 -0300

    Validación de usuarios ajustada a los requerimientos del sprint

--- src/middlewares/validateRegister.js
+++ src/middlewares/validateRegister.js
@@ -2,11 +2,12 @@ const path = require("path");
 const { body } = require("express-validator"); 
 
 const validations = [
-    body("firstName").notEmpty().withMessage("El nombre es obligatorio"),
-    body("lastName").notEmpty().withMessage("El apellido es obligatorio"),
-    body("logUser").notEmpty().withMessage("El nombre de usuario es obligatorio").isLength({ max: 8 }).withMessage("El nombre de usuario no debe tener más de 8 caracteres"),
+    body("firstName").notEmpty().withMessage("El nombre es obligatorio").isLength({min: 2}).withMessage("El nombre debe tener al menos 2 caracteres"),
+    body("lastName").notEmpty().withMessage("El apellido es obligatorio").isLength({min: 2}).withMessage("El apellido debe tener al menos 2 caracteres"),
+    body("logUser").notEmpty().withMessage("El nombre de usuario es obligatorio").isLength({min: 2}).withMessage("El nombre de usuario debe tener al menos 2 caracteres").isLength({ max: 8 }).withMessage("El nombre de usuario no debe tener más de 8 caracteres"),
     body("email").notEmpty().withMessage("El email es obligatorio").bail().isEmail().withMessage("Debes escribir el email en un formato válido"),
-    body("password").notEmpty().withMessage("La contraseña es obligatoria").bail().isLength({ min: 6 }).withMessage("La contraseña debe tener al menos 6 caracteres"),
+    //incluir mayusculas, minusculas, numeros y caracteres especiales
+    body("password").notEmpty().withMessage("La contraseña es obligatoria").bail().isLength({ min: 8 }).withMessage("La contraseña debe tener al menos 8 caracteres").matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}/).withMessage("La contraseña debe tener al menos una mayúscula, una minúscula, un número y un caracter especial"),
     body("repeat_password").notEmpty().withMessage("Debe repetir su contraseña").custom((value, { req }) => {
         if (value !== req.body.password) {
             throw new Error("Las contraseñas no coinciden");
@@ -16,7 +17,7 @@ const validations = [
     body("category_id").notEmpty().withMessage("La categoría es obligatoria"),
     body("image").custom((value, { req }) => {
         let file = req.file;
-        let acceptedExtensions = [".jpg", ".jpeg", ".png", ".JPG", ".JPEG", ".PNG"];
+        let acceptedExtensions = [".jpg", ".jpeg", ".gif", ".png", ".JPG", ".JPEG", ".PNG", ".GIF"];
         if (!file) {
             req.file = "user_01.png";
         } else { 
