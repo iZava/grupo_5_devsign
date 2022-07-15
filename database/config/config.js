04ce4b5817a599609239bbc7332792e310ac6f51
Isabel Rojas <isabel.rojasi@outlook.com>
2022-07-15 07:18:59 -0500

    package-json conflict solved

--- database/config/config.js
+++ database/config/config.js
@@ -1,7 +1,7 @@
 module.exports ={
   "development": {
     "username": "root",
-    "password": "0000",
+    "password": "123456",
     "database": "devsign_db",
     "host": "127.0.0.1",
     "dialect": "mysql"
